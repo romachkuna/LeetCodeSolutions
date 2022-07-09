635f803bd999f6d1bd7d0bdcc64e526e55aba447
romachkuna <romachkuna@gmail.com>
2022-07-09 00:21:16 -0700

    new solution , addTwoNumbers

---
+++ src/Solutions.java
@@ -0,0 +1,40 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Solutions {
+
+    public static void main(String[] args) {
+        ListNode ls = new ListNode(9,new ListNode(2));
+        ListNode x = new ListNode(8,new ListNode(3));
+        System.out.println(addTwoNumbers(ls,x).val);
+        System.out.println(addTwoNumbers(ls,x).next.val);
+    }
+
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode p, head = new ListNode();
+        p = head;
+        while (l1 != null || l2 != null || carry != 0) {
+            if (l1 != null) {
+                carry += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                carry += l2.val;
+                l2 = l2.next;
+            }
+            p.next = new ListNode(carry%10);
+            carry /= 10;
+            p = p.next;
+        }
+        return head.next;
+
+
+    }
+
+
+}
